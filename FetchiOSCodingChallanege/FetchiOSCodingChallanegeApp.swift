643932276239f21910fe22590996572a815f7b12
Tommy Chavez <tommychavez12@gmail.com>
2024-09-04 16:05:21 -0700

    Initial Commit

---
+++ FetchiOSCodingChallanege/FetchiOSCodingChallanegeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchiOSCodingChallanegeApp.swift
+//  FetchiOSCodingChallanege
+//
+//  Created by rnd2019 on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchiOSCodingChallanegeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchiOSCodingChallanegeUITests/FetchiOSCodingChallanegeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FetchiOSCodingChallanegeUITestsLaunchTests.swift
+//  FetchiOSCodingChallanegeUITests
+//
+//  Created by rnd2019 on 9/4/24.
+//
+
+import XCTest
+
+final class FetchiOSCodingChallanegeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
